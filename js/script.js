cff3ce3d824f725e06c7130107de2cb299146a4e
MN03SGO <146805872+MN03SGO@users.noreply.github.com>
2026-05-21 01:07:58 -0600

    Arreglo de btn proyectos e intento de implementacion de btn menu filtro

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-const menuIcono = document.querySelector('#menu-icon');
-const navEnlaces = document.querySelector('.nav_enlaces');
-
+const menuIcono = document.querySelector("#menu-icon");
+const navEnlaces = document.querySelector(".nav_enlaces");
 
 menuIcono.onclick = () => {
-    navEnlaces.classList.toggle('active');
-}
+  navEnlaces.classList.toggle("active");
+};
+
+const proyectos = [
+  {
+    nomProyecto: "Pagina web INTI",
+    img: "./img/img_proyectos/instituto.png",
+    category: 'proyectos web'
+  },
+  {
+    nomProyecto: "Florecer Natural",
+    img: "./img/img_proyectos/florecer.png",
+    category: 'proyectos web'
+  },
+  {
+    nomProyecto: "Joyeria Online",
+    img: "./img/img_proyectos/joyeria.png",
+    category: 'proyectos web'
+  },
+  {
+    nomProyecto: "Asistencia de viaje",
+    img: "./img/img_proyectos/fly.png",
+    category: 'proyectos web'
+  },
+  {
+    nomProyecto: "Asistente visual RPI 4",
+    category: 'proyectos embebidos'
+  }
+];
